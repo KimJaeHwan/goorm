e8ddb723a7ad6d82644972f68c952c96a4a06a49
KimJaeHwan <rfkjh22@naver.com>
2021-05-21 05:27:00 -0400

    add thread chatting room

--- network_programming/week_12/server_chat.c
+++ network_programming/week_12/server_chat.c
@@ -7,8 +7,7 @@
 #include <sys/time.h>		// timeout안쓰면 삭제해도 됨
 #include <sys/select.h>
 #include <sys/shm.h>
-
-void error_handling(char * message);
+#include <pthread.h>
 
 #define MAX_ROOM 4	// 대기실 1개 채팅룸 3개
 #define MAX_USER 10
@@ -16,38 +15,60 @@ void error_handling(char * message);
 #define KEY_NUM 1234
 
 struct chatting_room{
-	int pid;
+	int room_num;
 	int users[MAX_CAPA];
 	int user_cnt;
 };
 
+void * handle_clnt(void * arg);
+void send_msg(char *msg, int str_len, struct chatting_room *chat);
+void error_handling(char * message);
+int maxArr(int * arr, int size);
+
+int test = 0;
+
+
+/* thread vlaues */
+int clnt_sock;
+pthread_mutex_t mutx;
+
 int main(int argc, char * argv[])
 {
-	int serv_sock, clnt_sock;
+	int serv_sock;
 	struct sockaddr_in serv_adr, clnt_adr;
 	struct timeval timeout;
 	fd_set reads, cpy_reads;
 
 	socklen_t clnt_adr_sz;
 
+	/* select value */
 	int fd_max, str_len, fd_num, i,j;
+	
 	char message[BUFSIZ];
 	char menu_list[200] = "<MENU>\n1.채팅방 목록보기\n2. 채팅방 참여하기(사용법 : 2 <채팅방 번호>)\n3. 프로그램종료\n(0을 입력하면 메뉴가 다시 표시됩니다)\n";
 
 
 	//struct chatting_room chat[MAX_ROOM];
 	struct chatting_room *chatp;
+	
+	/* thread values */
+	pthread_t t_id;
+	
+	int chat_room_num;
+	/* shared memory values
 	int shm_id;
 	void * shm_addr;
-
 	int pid;
+	*/
 
 	if(argc!=2){
 		printf("Usage : %s <port>\n",argv[0]);
 		exit(1);
 	}
 	
-	int chat_room_num;
+	//int chat_room_num;
+	
+	pthread_mutex_init(&mutx, NULL);
 
 	serv_sock = socket(AF_INET, SOCK_STREAM, 0);
 	memset(&serv_adr, 0 , sizeof(serv_adr));
@@ -61,43 +82,23 @@ int main(int argc, char * argv[])
 	if(listen(serv_sock, 5) == -1)
 		error_handling("listen() error");
 
-	
-	/*
-	 *  공유 메모리 세팅 해당 공유 메모리는 PID와 user들의 소켓스크립터, user들의 수를 생각하고 있다.
-	 *  사이즈는 총 4개를 만들것이며 0번은 server부모 프로세스(대기실)로 사용되며 나머지는 채팅 룸이
-	 *  될것이다.
-	*/
-	
-	shm_id = shmget( (key_t)KEY_NUM, sizeof(struct chatting_room) * MAX_ROOM, IPC_CREAT | 0666);
-	shm_addr = shmat( shm_id, (void *)0, 0);
+	/* 기본 chatting room 3개 생성 + 대기실 */
+	chatp = (struct chatting_room *)calloc(sizeof(struct chatting_room),MAX_ROOM);
 
-	chatp = (struct chatting_room *)shm_addr;
-	
+
 	/* 서버(대기실)의 상태 정보 초기화*/
-	chat_room_num = 0;
-	chatp[0].pid = 1111;
+	//chat_room_num = 0;
+	//chatp[0].pid = 1111;
+	chatp[0].room_num = 0;
 	chatp[0].user_cnt = 0;
 
-
-	for( i = 1;i<MAX_ROOM;i++){			// 3개의 자식 프로세스 생성
-		pid = fork();
-
-		if(pid ==  0 ){	// 포크된 결과가 자식이면 그만
-			chat_room_num = i;
-			break;
-		}else{		// 부모 프로세스 인경우 pid 저장
-			chatp[i].pid = pid;
-		}
+	for(i = 1;i<MAX_ROOM;i++){
+		chatp[i].room_num = i;
+		pthread_create(&t_id, NULL, handle_clnt,(void*)&chatp[i]);
 	}
-		
-	if(pid == 0){			// 자식 프로세스
-		close(serv_sock);
-		printf("Hello I'm child process room_numver : %d ,PID : %d\n",chat_room_num,chatp[chat_room_num].pid);
-	}
-	else{			// 부모 프로세스
-		FD_ZERO(&reads);
-		FD_SET(serv_sock, &reads);
-		fd_max = serv_sock;
+	FD_ZERO(&reads);
+	FD_SET(serv_sock, &reads);
+	fd_max = serv_sock;
 	while(1)
 	{
 		cpy_reads = reads;
@@ -117,6 +118,8 @@ int main(int argc, char * argv[])
 				{
 					clnt_adr_sz = sizeof(clnt_adr);
 					clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+					chatp[0].users[chatp[0].user_cnt] = clnt_sock;
+					chatp[0].user_cnt++;
 					FD_SET(clnt_sock, &reads);
 					if(fd_max < clnt_sock)
 						fd_max = clnt_sock;
@@ -140,20 +143,6 @@ int main(int argc, char * argv[])
 						message[str_len] = 0;
 						printf("MSG from client %d : %s\n",i,message);
 						
-						/* broad cast other user
-
-
-						for(j = 4; j < fd_max +1; j++)
-						{
-							send(j,message,str_len,0);
-							printf("send client %d\n",j);
-						}
-						
-						printf("fd_max : %d\n", fd_max);
-						send(i,message,str_len,0);
-
-						*/
-
 						switch(message[0]){
 							case '0':
 								send(clnt_sock,menu_list,strlen(menu_list),0);
@@ -168,6 +157,24 @@ int main(int argc, char * argv[])
 								send(clnt_sock, message,strlen(message), 0);
 								break;
 							case '2':
+								if(fd_max == clnt_sock)		// 현재 fd_max값이 채팅방에 참여하고자 하는 clnt이면 값을 하나 내린다.
+									fd_max--;
+
+								FD_CLR(clnt_sock,&reads);	// 채팅방에 참여하고자하는 clnt를 select 검사에서 삭제
+								/* n번 채팅방 참여 시도 n번이 현재 존재하는 채팅방인지 체크하는 부분이 필요할듯*/						
+								chat_room_num = atoi(message + 2);
+								pthread_mutex_lock(&mutx);
+
+								printf("%d[[]]\n",chatp[chat_room_num].user_cnt);
+								chatp[chat_room_num].users[chatp[chat_room_num].user_cnt++] = clnt_sock;
+								test++;	// 이곳을 지나면 더이상 서버 메인 프로세스에서는 입력을 받지 못한다.		그리고 왜인지모르겠는데 chatp[chat_room_num].user_cnt가 2이다...
+								printf("chat_room_num : %d %d]\n",chat_room_num,test);
+								
+								pthread_mutex_unlock(&mutx);
+
+								//pthread_create(&t_id, NULL, handle_clnt,(void*)&chatp[chat_room_num]);						
+								printf("채팅방 %d번입장 \n",chat_room_num);
+								
 								break;
 							case '3':
 								break;
@@ -178,11 +185,93 @@ int main(int argc, char * argv[])
 			}
 		}
 	}
-	}
+//	}
 	close(serv_sock);
 	return 0;
 }
+int maxArr(int * arr, int size)
+{
+	int i, max = -1;
+	for( i = 0; i < size; i++) { if(max < arr[i]) max = arr[i]; }
+	return max;
+}
+void * handle_clnt(void *arg)
+{
+	int str_len = 0;
+	int chat_room_num;
+	int i, fd_max = -1, fd_num;
+	struct timeval timeout;
+	fd_set reads, cpy_reads;
+	int * users;
+	int user_cnt;
+	//chat_room_num = *((int*) arg);
+	char message[BUFSIZ];
+	struct chatting_room* chat;
 
+	chat = (struct chatting_room *)arg;
+
+	printf("Thread chatting room open [%d]\n",chat->room_num);
+
+	pthread_mutex_lock(&mutx);
+	users = chat->users;
+	user_cnt = chat->user_cnt;
+	FD_ZERO(&reads);
+	pthread_mutex_unlock(&mutx);
+
+	while(1){
+	pthread_mutex_lock(&mutx);
+	printf("[Ch. %d user_cnt : %d\n",chat->room_num,chat->user_cnt);
+	printf("Test : %d\n",test);
+	for(i = 0;i < user_cnt; i++)
+	{
+		FD_SET(users[i],&reads);
+		if( fd_max < users[i]) 
+			fd_max = users[i];
+		printf("[Ch. %d user : %d\n",chat->room_num,users[i]);
+	}
+	pthread_mutex_unlock(&mutx);
+	
+	cpy_reads = reads;
+	timeout.tv_sec = 1;
+	timeout.tv_usec = 1000;
+	if((fd_num = select(fd_max +1,&cpy_reads, 0 , 0, &timeout)) == -1)
+		break;
+	if(fd_num == 0)	// timeout
+		continue;
+	for( i = 0; i < user_cnt; i++)
+	{
+		if(FD_ISSET(users[i],&cpy_reads))
+		{
+			while((str_len=recv(users[i], message,BUFSIZ,0))!= 0)
+				send_msg(message,str_len, chat);
+			message[str_len] = 0;
+			printf("[Ch. %d] message %s\n",chat->room_num, message);	
+			pthread_mutex_lock(&mutx);
+			if(str_len == 0)		// 'quit 와 같은 해제 문자 입력시 탈출
+			{
+				FD_CLR(users[i],&reads);
+				chat->users[chat->user_cnt++] = users[i];
+				while(i++<user_cnt+1)	// 배열 요소에서 제거
+					users[i] = users[i + 1];
+				user_cnt--;
+				chat->user_cnt = user_cnt;
+			}
+			pthread_mutex_unlock(&mutx);
+		}
+	}
+
+	}
+}
+
+void send_msg(char * msg,int str_len, struct chatting_room *chat)
+{
+	int i;
+
+	pthread_mutex_lock(&mutx);
+	for(i = 0; i < chat->user_cnt; i++)
+		send(chat->users[i], msg, str_len, 0);
+	pthread_mutex_unlock(&mutx);
+}
 void error_handling(char * message)
 {
 	fputs(message, stderr);
